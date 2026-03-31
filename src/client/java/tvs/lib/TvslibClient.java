b4b2443ea5cabf8c981a42bade68d58176d09a83
tvs-vin <bradgaw007@gmail.com>
2026-03-31 14:46:43 -0400

    Adds in basic settup

---
+++ src/client/java/tvs/lib/TvslibClient.java
@@ -0,0 +1,10 @@
+package tvs.lib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TvslibClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
